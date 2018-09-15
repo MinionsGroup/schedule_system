c78bc69f89c14f71c43a5f16edd727e1009d5038
noemiguzo <noemi.guzo.ju@gmail.com>
2018-09-15 15:45:22 -0400

    rest

---
+++ src/main/java/schedule_system/schedule_system/Student/Student.java
@@ -0,0 +1,58 @@
+package schedule_system.schedule_system.Student;
+
+import java.util.concurrent.ThreadLocalRandom;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+@Data
+@Entity
+public class Student {
+
+    private @Id @GeneratedValue Long id;
+    private int code;
+    private String name;
+    private String lastName;
+    private int age;
+
+
+
+    public Student()
+    {
+        super();
+    }
+    public Student(long id,int code, String name, String lastName, int age)
+    {
+        super();
+        this.id = id;
+        this.code = code;
+        this.name = name;
+        this.age = age;
+    }
+    public Student( String name)
+    {
+        this.code = ThreadLocalRandom.current().nextInt(2000, 2500);
+        this.name = name;
+        this.age = ThreadLocalRandom.current().nextInt(5, 50);;
+    }
+    public long getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public long getAge()
+    {
+        return age;
+    }
+
+
+    @Override
+    public String toString()
+    {
+        return "Student [id=" + id + ", name=" + name + ", age=" + age + "]";
+    }
+}
---
+++ src/test/java/schedule_system/schedule_system/Student/StudentControllerTest.java
@@ -0,0 +1,7 @@
+package schedule_system.schedule_system.Student;
+
+import static org.junit.Assert.*;
+
+public class StudentControllerTest {
+
+}
